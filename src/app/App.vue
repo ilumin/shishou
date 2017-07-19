d30a4d7adc077ab4cd32d6f9dba2facd73fde007
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-07-19 09:15:53 +0700

    UPDATE move app into module

--- src/app/App.vue
+++ src/app/App.vue
@@ -4,7 +4,8 @@
       <span>Vue.js PWA</span>
     </header>
     <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
+      asdasd
+      <img src="../assets/logo.png" alt="Vue.js PWA">
       <router-view></router-view>
     </main>
   </div>
